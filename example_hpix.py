047b6c0d23cf52698846e5e154dab0cf5a915a5a
Joan Rue Queralt <joan.rue.q@gmail.com>
2023-02-02 15:54:11 +0100

    [example_hpix] update WIP

--- example_hpix.py
+++ example_hpix.py
@@ -17,15 +17,23 @@ try:
     from ska_sdp_func_python.util.coordinate_support import skycoord_to_lmn
 
     import healpy as hp
+    import astropy_healpix
+    from astropy_healpix import HEALPix
+    import healpy as hp
+
+    from astropy.coordinates import Galactic
+    from astropy.io import fits
 except:
     raise ValueError("Missing packages")
 
 import hvox
 
 if __name__ == "__main__":
-
+    ra = 15.0 * u.deg
+    dec = -45.0 * u.deg
+    rad = 5.0 * u.deg
     print("Allocating visibilities / Creating Low-SKA configuration")
-    lowr3 = create_named_configuration("LOWBD2", rmax=3_000.0)
+    lowr3 = create_named_configuration("LOWBD2", rmax=6_00.0)
     vis = create_visibility(
         config=lowr3,
         times=np.linspace(-4, 4, 9) * np.pi / 12,
@@ -33,7 +41,7 @@ if __name__ == "__main__":
         channel_bandwidth=np.r_[5e4],
         weight=1.0,
         phasecentre=SkyCoord(
-            ra=+15.0 * u.deg, dec=-45.0 * u.deg, frame="icrs", equinox="J2000"
+            ra=ra , dec=dec, frame="icrs", equinox="J2000"
         ),
         polarisation_frame=PolarisationFrame("stokesI"),
         times_are_ha=False,
@@ -42,16 +50,34 @@ if __name__ == "__main__":
     print("Loading sky GT")
     hvox_path = os.sep.join(os.path.abspath(hvox.__file__).split(os.sep)[:-3])
     fits_path = os.path.join(hvox_path, "data", "models", "haslam408_dsds_Remazeilles2014.fits")
-    haslam, header = hp.read_map(fits_path, h=True)
 
-    direction_cosines, jacobian = get_direction_cosines(haslam)
-    wgt_dirty = 1 / jacobian.reshape(-1)
+    # Load fits
+    hdulist = fits.open(fits_path)
+    nside = hdulist[1].header['NSIDE']
+    order = hdulist[1].header['ORDERING']
+    # Create healpix object
+    haslam_hpix = HEALPix(nside=nside, order=order, frame=Galactic())
+    # get cone of interest
+    haslam_coi = haslam_hpix.cone_search_lonlat(ra, dec, radius=rad)
+    # get map values
+    haslam_full = hp.read_map(fits_path)
+    # get values of interest
+    haslam = haslam_full[haslam_coi]
+    # get ra and dec (lat and lon) coordinaes
+    directions = haslam_hpix.healpix_to_skycoord(haslam_coi)
+    # convert o lmn
+    lmn = skycoord_to_lmn(directions, SkyCoord(ra=ra, dec=dec, frame="icrs", equinox="J2000"))
+    lmn = np.asarray(lmn).T
+
+    xyz = lmn
+    # dont use jacobian for healpix
+    wgt_dirty = None # 1 / jacobian
 
     print("Simulating visibilities")
     vis_estimate = hvox.dirty2vis(
         uvw_lambda=vis.visibility_acc.uvw_lambda.reshape(-1, 3),
-        xyz=direction_cosines.reshape(-1, 3),
-        dirty=m31image.pixels.data.reshape(-1),
+        xyz=xyz.reshape(-1, 3),
+        dirty=haslam.reshape(-1),
         wgt_dirty=wgt_dirty,
         chunked=True,
     )
@@ -60,33 +86,60 @@ if __name__ == "__main__":
     sky_estimate = (
         hvox.vis2dirty(
             uvw_lambda=vis.visibility_acc.uvw_lambda.reshape(-1, 3),
-            xyz=direction_cosines.reshape(-1, 3),
+            xyz=xyz.reshape(-1, 3),
             vis=vis_estimate.reshape(-1),
             wgt_vis=vis.visibility_acc.flagged_weight.reshape(-1),
             wgt_dirty=wgt_dirty,
             chunked=True,
         )
-        .reshape(m31image.pixels.shape)
-        .squeeze()
     )
     print("Estimating PSF")
     psf_estimate = (
         hvox.compute_psf(
             uvw_lambda=vis.visibility_acc.uvw_lambda.reshape(-1, 3),
-            xyz=direction_cosines.reshape(-1, 3),
+            xyz=xyz.reshape(-1, 3),
             wgt_vis=vis.visibility_acc.flagged_weight.reshape(-1),
-            wgt_dirty=wgt_dirty,
+            wgt_psf=wgt_dirty,
             chunked=True,
         )
-        .reshape(m31image.pixels.shape)
-        .squeeze()
     )
+    fig, ax = plt.subplots(
+        figsize=(4, 4),
+        subplot_kw={"projection": "3d"},
+    )
+    ids = np.random.permutation(len(xyz))[:1000]
+    ax.scatter(*xyz[ids].T, alpha=0.1)
+    plt.show()
+
+    haslam_plot = hp.ma(haslam_full)
+    haslam_plot.mask = np.ones_like(haslam_full, dtype=bool)
+    haslam_plot.mask[haslam_coi] = False
+
+
+    dirty_plot = haslam_plot.copy()
+    dirty_plot[haslam_coi] = sky_estimate
+
+    psf_plot = haslam_plot.copy()
+    psf_plot[haslam_coi] = psf_estimate
 
     hp.mollview(
-        haslam,
+        abs(haslam_plot),
         norm="log",
     )
     hp.graticule()
     plt.show()
 
+    hp.mollview(
+        abs(dirty_plot),
+        norm="log",
+    )
+    hp.graticule()
+    plt.show()
+
+    hp.mollview(
+        abs(psf_plot),
+        norm="log",
+    )
+    hp.graticule()
+    plt.show()
     print("Done!")
